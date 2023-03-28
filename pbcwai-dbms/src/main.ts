94157c171f7620b5b0324c064950047ef17acbd0
Darren Duncan <darren@DarrenDuncan.net>
2023-03-28 15:10:26 -0700

    pbcwai-dbms : explicitly use NestExpressApplication and global url prefix of /api

--- pbcwai-dbms/src/main.ts
+++ pbcwai-dbms/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Nest Application with Express Platform.
+  const app: NestExpressApplication
+    = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Assign global url path prefix to all endpoints.
+  app.setGlobalPrefix('/api');
+
+  // Listen on localhost port 3000.
   await app.listen(3000);
 }
+
 bootstrap();
