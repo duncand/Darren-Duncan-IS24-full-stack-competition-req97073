25cd74acaf720dbfb6fe57a08f90a44665989a3a
Darren Duncan <darren@DarrenDuncan.net>
2023-03-28 22:05:17 -0700

    pbcwai-dbms : first pass at adding Swagger endpoints

--- pbcwai-dbms/src/main.ts
+++ pbcwai-dbms/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -9,7 +10,21 @@ async function bootstrap() {
     = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Assign global url path prefix to all endpoints.
+  // So then otherwise-unqalified "products" is under /api/products and so on.
   app.setGlobalPrefix('/api');
+
+  // Generate Swagger docs from the implementing code.
+  // This produces multiple endpoints, each with a different format of the docs:
+  //    /api/api-docs
+  //    /api/api-docs-json
+  //    /api/api-docs-yaml
+  const config = new DocumentBuilder()
+    .setTitle('pbcwai-dbms')
+    .setVersion('0.0.1')
+    .setDescription('Province of British Columbia - Web Application Inventory - DBMS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/api-docs', app, document);
 
   // Listen on localhost port 3000.
   await app.listen(3000);
