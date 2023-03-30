c2101b328cc73b3d1928929170fdbbff3bc85c35
Darren Duncan <darren@DarrenDuncan.net>
2023-03-30 16:55:18 -0700

    pbcwai-dbms : remove never-used Product "entity"

--- pbcwai-dbms/src/products/entities/product.entity.ts
+++
@@ -1,9 +0,0 @@
-export class Product {
-  productNumber: string;
-  productName: string;
-  scrumMasterName: string;
-  productOwnerName: string;
-  developerNames: Array<string>;
-  startDate: string;
-  methodology: string;
-}
